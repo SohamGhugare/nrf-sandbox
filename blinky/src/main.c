a98becc8fc5932f2c052ed07719377fc472870eb
Soham Ghugare <soham.ghugare@gmail.com>
2024-05-23 15:31:44 +0530

    feat: added ncs-fund

--- blinky/src/main.c
+++ blinky/src/main.c
@@ -8,36 +8,50 @@
 #include <zephyr/drivers/gpio.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define SLEEP_TIME_MS   100
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
 
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+/* this struct contains the device information like pointer to device, pin number, etc */
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+
 
 int main(void)
 {
 	int ret;
 
-	if (!gpio_is_ready_dt(&led)) {
+	/* checking if the gpio pin is ready to use */
+	if (!gpio_is_ready_dt(&led0)) {
 		return 0;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (!gpio_is_ready_dt(&led1)) {
+		return 0;
+	}
+
+	/* configuring the gpio pin */
+	ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return 0;
 	}
 
+	ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	int val = 0;
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
-		}
+		gpio_pin_set_dt(&led0, val);
+		gpio_pin_set_dt(&led1, !val);
+
 		k_msleep(SLEEP_TIME_MS);
+
+		val = !val;
 	}
 	return 0;
 }
